1ce2ff1925d280cc3d30cbb23d48389d78d1d3bf
ST10262759 <126753536+ST10262759@users.noreply.github.com>
2024-07-25 09:02:41 +0200

    Add project files.

---
+++ index/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace index
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee(40000, "Manager", "Charles", "Mathews", "EMP0111");
+
+            Console.WriteLine(emp[0]);
+            Console.WriteLine(emp[1]);
+            Console.WriteLine(emp[2]);
+            Console.WriteLine(emp[3]);
+            Console.WriteLine(emp[4]);
+
+            // Accessing using string attrName
+            Console.WriteLine("employee Salary: " + emp["Salary"]);
+            Console.WriteLine("employee Number: " + emp["empNumber"]);
+            Console.WriteLine("employee JobTitle: " + emp["jobTitle"]);
+            Console.WriteLine("employee Name: " + emp["Name"]);
+            Console.WriteLine("employee Surname: " + emp["surname"]);
+        }
+    }
+}
